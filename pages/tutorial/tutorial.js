037b164df7bbf5eedcb13d35854999fb29ecb5be
zhanglainghao <15821881675@163.com>
2019-12-14 16:32:11 +0800

    增加教程页面

---
+++ pages/tutorial/tutorial.js
@@ -0,0 +1,79 @@
+// pages/tutorial/tutorial.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    steps: [
+      {
+      name:'第一步：微信搜索 勃朗圣泉',
+        url:'../../static/images/tutorial/step1.png'
+      },
+      {
+        name: '第二步：点击公众号',
+        url: '../../static/images/tutorial/step2.png'
+      },
+      {
+        name: '第三步：关注我们',
+        url: '../../static/images/tutorial/step3.png'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
