c4de4097e4028eb678dfa3aa9f8fbf3253894233
zhanglainghao <15821881675@163.com>
2019-12-21 13:00:58 +0800

    feat: 增加获券页面

--- pages/getCoupons/getCoupons.js
+++ pages/getCoupons/getCoupons.js
@@ -1,66 +1,78 @@
-// pages/getCoupons/getCoupons.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coupons: [{
+        title: '新人福利',
+        value: 5,
+        condition: '无门槛使用'
+      },
+      {
+        title: '会员专享',
+        value: 20,
+        condition: '无门槛使用'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+  },
+  getCoupons(e){
+    console.log('领取价值:' + e.target.dataset.value + '的券！')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
